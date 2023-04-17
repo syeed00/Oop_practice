ec56412cd39d4386d5433c3c04b74bf68aef0177
syeed00 <88630625+syeed00@users.noreply.github.com>
2023-04-17 15:21:17 +0600

    Add files via upload

---
+++ Encapsulation_prac/person.java
@@ -0,0 +1,21 @@
+package Encapsulation;
+
+public class person {
+    private String name;
+    private int age;
+
+    public void setnam(String name){
+        this.name=name;
+    }
+
+    public String getnam(){
+        return name;
+    }
+
+    public void setage(int age){
+        this.age=age;
+    }
+    public int getage(){
+        return age;
+    }
+}
---
+++ Encapsulation_prac/testencap.java
@@ -0,0 +1,12 @@
+package Encapsulation;
+
+public class testencap {
+    public static void main(String[] args) {
+
+        person p1=new person();
+        p1.setnam("syeed");
+        p1.setage(25);
+        System.out.println(p1.getnam());
+        System.out.println(p1.getage());
+    }
+}
